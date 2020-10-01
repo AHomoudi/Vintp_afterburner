5a6c6f8e4cc1f1f1cbe25e9a5739d9bb18e6700f
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2020-10-01 11:07:40 +0200

    main Vintp_afterburner

---
+++ main_CanESM2_IPSL-CM5A-MR.R
@@ -0,0 +1,107 @@
+#===============================================================================
+#This code is part of Master thesis work, it is written to vertically interpolate
+# CMIP5 dataset on model level (hybrid sigma level) to pressure level.
+
+#After : 
+#Trenberth, K. E., Berry, J. C., & Buja, L. E. (1993). Vertical Interpolation 
+#and Truncation of Model-Coordinate Data. NCAR Tech. Note TN-396+STR, 54 pp.
+
+#ECMWF. (2014). IFS DOCUMENTATION – Cy40R1, Operational implementation 22 Novem-
+#ber 2013, PART VII : Wave Model. ECMWF IFS Documentation, November 2013, 1–79.
+
+
+                          #Author: Ahmed.A.B.Homoudi
+                             #Date:October 2020
+#CMIP5 models that can be processed with code : 
+   #CCCma-CanESM2
+   #IPSL-IPSL-CM5A-MR
+#CMIP5 models that can not be processed with code : 
+  #MOHC-HadGEM2-ES
+  #CNRM-CERFACS-CNRM-CM5
+  #CSIRO-QCCCE-CSIRO-Mk3-6-0
+  #ICHEC-EC-EARTH
+  #MIROC-MIROC5
+  #NCC-NorESM1-M
+  #NOAA-GFDL-GFDL-ESM2M
+  #MPI-M-MPI-ESM-LR
+  #MPI-M-MPI-ESM-MR
+
+
+#1==============================================================================
+library(stringr)
+
+#2==============================================================================
+#Listing all files in directory
+u_nc.files<- paste0("u_wind/",list.files(path = "u_wind/", pattern = ".nc$"))
+v_nc.files<- paste0("v_wind/",list.files(path = "v_wind/", pattern = ".nc$"))
+
+q_nc.files<- paste0("q/",list.files(path = "q/", pattern = ".nc$"))
+t_nc.files<- paste0("t/",list.files(path = "t/", pattern = ".nc$"))
+#2==============================================================================
+#Reading surface geopotential files.
+z_nc.files<- paste0("surface/",list.files(path = "surface/", pattern = ".nc$"))
+
+
+#3==============================================================================
+# Eastward wind vertical interpolation
+
+source("wind_afterburner/wind_afterburner_CanESM2_IPSL-CM5A-MR.R")
+
+required_PLev<-seq(85000,100000,2500) #Pa 
+
+for (x in 1:length(u_nc.files)){
+  Wind_afterburner(wind_file = u_nc.files[x],
+                   req_press_levels = required_PLev)
+  
+}
+
+#4==============================================================================
+# Northward wind vertical interpolation 
+source("wind_afterburner/wind_afterburner_CanESM2_IPSL-CM5A-MR.R")
+
+required_PLev<-seq(85000,100000,2500) #Pa 
+
+for (x in 1:length(v_nc.files)){
+  Wind_afterburner(wind_file = v_nc.files[x],
+                   req_press_levels = required_PLev)
+  
+}
+#5==============================================================================
+# Specific humidity and temperature vertical interpolation 
+
+required_PLev<-85000   #Pa
+
+source("spec_hum_afterburner/spec_hum_afterburner_CanESM2_IPSL-CM5A-MR.R")
+
+for (x in 1:length(q_nc.files)){
+  
+  Specific_humidity_afterburner(spec_hum_file = q_nc.files[x],
+                                req_press_levels = required_PLev)
+}
+
+#6==============================================================================
+# Temperature vertical interpolation
+
+required_PLev<-85000   #Pa
+
+source("temp_afterburner/temp_afterburner_CanESM2_IPSL-CM5A-MR.R")
+
+for (x in 1:length(t_nc.files)){
+  
+  file_model<-unlist(str_split(unlist(str_split(t_nc.files[x], "[/]"))[2],"_"))[3]
+  
+  surface_geopotential <-grep(file_model, z_nc.files, value=TRUE, fixed=TRUE)
+  
+  Temperature_afterburner(temp_file =t_nc.files[x],
+                          surf_gepo = surface_geopotential,
+                          req_press_levels = required_PLev)
+}
+#=================================END===========================================
+#Test
+#u_nc.files<-u_nc.files[c(1,6)]
+#v_nc.files<- v_nc.files[c(1,6)]
+#t_nc.files<-t_nc.files[c(1,6)]
+#q_nc.files<-q_nc.files[c(1,6)]
+
+#z_nc.files<-z_nc.files[c(2,7)]
+
